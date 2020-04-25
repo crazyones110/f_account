0ce801c7a36537bee8d5fee866f4fd1624806288
fanluping <fanluping@bytedance.com>
2020-04-25 09:09:15 +0800

    导航栏初步完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,86 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom'
+import styled from 'styled-components'
 
-const Button = styled.button`
-  width: 100px;
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
+const Nav = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
 `
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Button>aaaaa</Button>
-    </div>
-  );
+    <Router>
+      <Wrapper>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
+  )
 }
 
-export default App;
+function Tags() {
+  return <h2>标签页</h2>
+}
+function Money() {
+  return <h2>记账页</h2>
+}
+
+function Statistics() {
+  return <h2>统计页</h2>
+}
+
+function NoMatch() {
+  return <div>404</div>
+}
