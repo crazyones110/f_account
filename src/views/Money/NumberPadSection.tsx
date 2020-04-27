cc794afca78f57ff6b2c6e6f21590fddb06ec136
fanluping <fanluping@bytedance.com>
2020-04-27 09:49:12 +0800

    feat: numberPad WIP

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import React, { useState } from 'react'
 
-export const NumberPadSection = styled.section`
+const StyledNumberPadSection = styled.section`
   display: flex;
   flex-direction: column;
   > .output {
@@ -10,7 +11,7 @@ export const NumberPadSection = styled.section`
     text-align: right;
     padding: 0 16px;
     box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
-    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+      inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
   }
   > .pad {
     > button {
@@ -30,16 +31,23 @@ export const NumberPadSection = styled.section`
       &:nth-child(1) {
         background: #f2f2f2;
       }
-      &:nth-child(2), &:nth-child(5) {
+      &:nth-child(2),
+      &:nth-child(5) {
         background: #e0e0e0;
       }
-      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
         background: #d3d3d3;
       }
-      &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
         background: #c1c1c1;
       }
-      &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
         background: #b8b8b8;
       }
       &:nth-child(12) {
@@ -51,3 +59,59 @@ export const NumberPadSection = styled.section`
     }
   }
 `
+
+export const NumberPadSection: React.FC = props => {
+  const [output, setOutput] = useState<string>('0')
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const content = (e.target as HTMLButtonElement).textContent
+    if (content === null) {return}
+    switch (content) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if (output === '0') {
+          setOutput(content)
+        } else {
+          setOutput(output + content)
+        }
+        break
+      case '删除':
+        console.log('删除')
+        break
+      case '清空':
+        console.log('清空')
+        break
+      case 'OK':
+        console.log('OK')
+        break
+    }
+  }
+  return (
+    <StyledNumberPadSection>
+      <div className="output">{output}</div>
+      <div className="pad clearfix" onClick={onClickButtonWrapper}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">OK</button>
+        <button className="zero">0</button>
+        <button>.</button>
+      </div>
+    </StyledNumberPadSection>
+  )
+}
